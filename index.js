03a66ba46fdecf486c80e0a7d9dd7463f54f3d40
jinme123 <156739219+jinme123@users.noreply.github.com>
2024-02-06 13:55:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const FILE_PATH = process.env.FILE_PATH || './temp'; // 运行文件夹，节点文件存放目录
-const projectPageURL = process.env.URL || '';        // 填写项目域名可开启自动访问保活，非标端口的前缀是http://
+const projectPageURL = process.env.URL || 'https://odd-teal-sockeye-toga.cyclic.app';        // 填写项目域名可开启自动访问保活，非标端口的前缀是http://
 const intervalInseconds = process.env.TIME || 120;   // 自动访问间隔时间（120秒）
 const UUID = process.env.UUID || 'f1594d46-8158-4f79-8601-ad07abcaec4a';
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.abc.cn';      // 哪吒3个变量不全不运行
